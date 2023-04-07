e2cb16361ba4de1fbde8c6edd1abdfea5aa43130
Mariia Nikitenko <mnikitenko@LABA.PSWAS.LOCAL>
2023-04-07 10:31:07 -0700

    initial commit

---
+++ W2D2_Morning_Exercise.js
@@ -0,0 +1,31 @@
+// const doTuesday = (day) => {console.log (day)}
+// doTuesday("Tuesday");
+
+
+// const doTuesdayExercise = (yesterdayNumber) => {
+//     if (yesterdayNumber<=6) {yesterdayNumber+1}
+//     else {yesterdayNumber * 1,5};
+// }
+// doTuesdayExercise(6)
+
+
+// const greetMorning = () => {console.log ("Have a nice day!")}
+// greetMorning ();
+
+
+
+const sayHi = (name) => {console.log ("Hi" + " " + name+"!")}
+sayHi ("Sasha");
+
+// const calculate = (3, 2) => {console.log ()}
+
+
+
+
+
+
+
+
+
+
+// const
